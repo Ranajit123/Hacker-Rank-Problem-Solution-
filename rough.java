f150ddbe3aae3926d812d0050e947fa67cc56beb
Ranajit Das <das143ranajit@gmail.com>
2024-01-18 01:05:55 +0530

    update gitignore

--- rough.java
+++ rough.java
@@ -1,17 +1,9 @@
-import java.text.NumberFormat;  
 import java.util.*;  
 public class rough {  
   
-static void printCurrency(Locale locale){  
- double dbl=10500.3245;  
- NumberFormat formatter=NumberFormat.getCurrencyInstance(locale);  
- String currency=formatter.format(dbl);  
- System.out.println(currency+" for the locale "+locale);  
-}  
+ 
   
 public static void main(String[] args) {  
-    printCurrency(Locale.UK);  
-    printCurrency(Locale.US);  
-    printCurrency(Locale.FRANCE);  
+    System.out.println((-5%1));
 }  
 }  
